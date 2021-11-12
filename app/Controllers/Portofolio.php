01edd16d86f4fd0f15c96000b0609ba68e81a029
yogafolio <siaganfox@gmail.com>
2021-11-12 19:46:32 +0700

    first

---
+++ app/Controllers/Portofolio.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Portofolio extends BaseController
+{
+    public function index()
+    {
+        return view('pages/portofolio');
+    }
+}
---
+++ app/Controllers/Pricing.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Pricing extends BaseController
+{
+    public function index()
+    {
+        return view('pages/pricing');
+    }
+}
